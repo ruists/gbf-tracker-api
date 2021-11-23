54e2ed005c80630903dcb7742e5868ad92daebd2
Rui Santos <clovias@gmail.com>
2021-11-23 12:36:20 +0000

    Added ownership verification to edit/delete methods

--- api/controllers/character.js
+++ api/controllers/character.js
@@ -7,107 +7,166 @@ exports.character_getAll = (req, res, next) => {
   Character.find({
     user: userId
   })
-    .select("-__v")
-    .lean({ autopopulate: true })
-    .exec()
-    .then((result) => {
-      const response = {
-        count: result.length,
-        characters: result.map((character) => {
-          return {
-            character,
-            request: {
-              type: "GET",
-              url:
-                req.protocol +
-                "://" +
-                req.get("host") +
-                "/character/" +
-                character._id
-            }
-          };
-        })
-      };
-      res.status(200).json(response);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err
-      });
+  .select("-__v")
+  .lean({ autopopulate: true })
+  .exec()
+  .then((result) => {
+    const response = {
+      count: result.length,
+      characters: result.map((character) => {
+        return {
+          character,
+          request: {
+            type: "GET",
+            url:
+            req.protocol +
+            "://" +
+            req.get("host") +
+            "/character/" +
+            character._id
+          }
+        };
+      })
+    };
+    res.status(200).json(response);
+  })
+  .catch((err) => {
+    res.status(500).json({
+      error: err
     });
+  });
 };
 
 exports.character_create = (req, res, next) => {
   BaseCharacter.findById(req.body.baseCharacterId)
-    .lean()
-    .exec()
-    .then((baseCharacter) => {
-      if (!baseCharacter) {
-        return res.status(500).json({
-          message: "Base character not found."
-        });
-      }
-
-      const character = new Character({
-        _id: new mongoose.Types.ObjectId(),
-        level: req.body.level,
-        uncap: req.body.uncap,
-        baseCharacter: baseCharacter._id
+  .lean()
+  .exec()
+  .then((baseCharacter) => {
+    if (!baseCharacter) {
+      return res.status(500).json({
+        message: "Base character not found."
       });
-      return character.save();
-    })
-    .then((result) => {
-      if (res.statusCode === 500) {
-        return res;
-      }
-      const response = {
-        message: "Created character successfully",
-        character: {
-          ...result.toJSON(),
-          request: {
-            type: "GET",
-            url:
-              req.protocol +
-              "://" +
-              req.get("host") +
-              "/character/" +
-              result._id
-          }
-        }
-      };
-      res.status(201).json(response);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err
-      });
+    }
+    
+    const character = new Character({
+      _id: new mongoose.Types.ObjectId(),
+      level: req.body.level,
+      uncap: req.body.uncap,
+      baseCharacter: baseCharacter._id,
+      user: req.userData.userId
     });
+    return character.save();
+  })
+  .then((result) => {
+    if (res.statusCode === 500) {
+      return res;
+    }
+    const response = {
+      message: "Created character successfully",
+      character: {
+        ...result.toJSON(),
+        request: {
+          type: "GET",
+          url:
+          req.protocol +
+          "://" +
+          req.get("host") +
+          "/character/" +
+          result._id
+        }
+      }
+    };
+    res.status(201).json(response);
+  })
+  .catch((err) => {
+    res.status(500).json({
+      error: err
+    });
+  });
 };
 
 exports.character_getCharacter = (req, res, next) => {
   const id = req.params.characterId;
   const userId = req.userData.userId;
   Character.findById(id)
-    .select("-__v")
-    .lean({ autopopulate: true })
-    .exec()
-    .then((result) => {
-      if (result) {
-        if (result.user !== userId) {
-          res.status(403).json({
-            message: "Unauthorized access to resource."
+  .select("-__v")
+  .lean({ autopopulate: true })
+  .exec()
+  .then((result) => {
+    if (result) {
+      if (result.user !== userId) {
+        res.status(403).json({
+          message: "Unauthorized access to resource."
+        });
+      } else {
+        const response = {
+          character: {
+            result
+          }
+        };
+        res.status(200).json(response);
+      }
+    } else {
+      res.status(404).json({
+        message: "Character not found."
+      });
+    }
+  })
+  .catch((err) => {
+    res.status(500).json({
+      error: err
+    });
+  });
+};
+
+exports.character_edit = (req, res, next) => {
+  const id = req.params.characterId;
+  const userId = req.userData.userId;
+  const updateOps = {};
+  const keys = Object.keys(req.body);
+  for (const key of keys) {
+    updateOps[key] = req.body[key];
+  }
+  
+  Character.findById(id)
+  .select("-__v")
+  .lean({autopopulate: true})
+  .exec()
+  .then((character) => {
+    if(character) {
+      if(character.user !== userId) {
+        res.status(403).json({
+          message: "Unauthorized access to resource."
+        });
+      } else {
+        Character.update(
+          {
+            _id: id
+          },
+          {
+            $set: updateOps
+          }
+          )
+          .exec()
+          .then((result) => {
+            const response = {
+              message: "Character updated.",
+              request: {
+                type: "GET",
+                url: req.protocol + "://" + req.get("host") + "/character/" + id
+              }
+            };
+            res.status(200).json(response);
+          })
+          .catch((err1) => {
+            res.status(500).json({
+              error: err1
+            });
           });
-        } else {
-          const response = {
-            character: {
-              result
-            }
-          };
-          res.status(200).json(response);
         }
       } else {
         res.status(404).json({
-          message: "Character not found."
+          error: "Character not found."
         });
       }
     })
@@ -116,59 +175,52 @@ exports.character_getCharacter = (req, res, next) => {
         error: err
       });
     });
-};
-
-exports.character_edit = (req, res, next) => {
-  const id = req.params.characterId;
-  const updateOps = {};
-  const keys = Object.keys(req.body);
-  for (const key of keys) {
-    updateOps[key] = req.body[key];
-  }
-  Character.update(
-    {
-      _id: id
-    },
-    {
-      $set: updateOps
-    }
-  )
+    
+  };
+  
+  exports.character_delete = (req, res, next) => {
+    const id = req.params.characterId;
+    const userID = req.userData.userId;
+    Character.findById(id)
+    .select("-__v")
+    .lean({autopopulate: true})
     .exec()
-    .then((result) => {
-      const response = {
-        message: "Character updated.",
-        request: {
-          type: "GET",
-          url: req.protocol + "://" + req.get("host") + "/character/" + id
+    .then((character) => {
+      if(character) {
+        if(character.user !== userId) {
+          res.status(403).json({
+            message: "Unauthorized access to resource."
+          });
+        } else {
+          Character.remove({
+            _id: id
+          })
+          .exec()
+          .then((result) => {
+            res.status(200).json({
+              message: "Character deleted.",
+              request: {
+                type: "GET",
+                url: req.protocol + "://" + req.get("host") + "/character/"
+              }
+            });
+          })
+          .catch((err) => {
+            res.status(500).json({
+              error: err
+            });
+          });
         }
-      };
-      res.status(200).json(response);
+      } else {
+        res.status(404).json({
+          error: "Character not found."
+        });
+      }
     })
     .catch((err) => {
       res.status(500).json({
         error: err
       });
     });
-};
-
-exports.character_delete = (req, res, next) => {
-  const id = req.params.characterId;
-  Character.remove({
-    _id: id
-  })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: "Character deleted.",
-        request: {
-          type: "GET",
-          url: req.protocol + "://" + req.get("host") + "/character/"
-        }
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err
-      });
-    });
-};
+  };
+  
